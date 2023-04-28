84d8e450ae172c23458cd1bde43bffdd0de6aec8
Ishan <ishanwijes@gmail.com>
2023-04-28 19:23:24 +1000

    added basic bootstrap navbar

---
+++ blinq-challenge-code/components/Navigation.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import navbarImage from '../public/navbar-image.png'
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+
+export default function Navigation() {
+  return (
+    <Navbar bg="light" expand="lg" className='navbar-container'>
+
+      <Container className='navbar-flex-container'>
+
+        <Link href='/'>
+            <Image
+              src={navbarImage}
+              alt='Company Logo'
+              width={90}
+              height={30}
+            />
+        </Link>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <Navbar.Collapse id="basic-navbar-nav">
+
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+
+        </Navbar.Collapse>
+
+      </Container>
+    </Navbar>
+  )
+}
--- blinq-challenge-code/pages/_app.tsx
+++ blinq-challenge-code/pages/_app.tsx
@@ -1,8 +1,20 @@
+import Head from 'next/head'
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps } from 'next/app'
 
+
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+       
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
