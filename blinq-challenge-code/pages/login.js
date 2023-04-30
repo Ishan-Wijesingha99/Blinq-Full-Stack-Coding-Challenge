7334611eddfee435e11a2cbefbda577222db70ee
Ishan <ishanwijes@gmail.com>
2023-04-30 20:56:21 +1000

    added page redirect when logging in and registering

--- blinq-challenge-code/pages/login.js
+++ blinq-challenge-code/pages/login.js
@@ -4,6 +4,8 @@ import { useState } from 'react'
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import { useAuth } from '../context/AuthContext';
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 
 
@@ -15,13 +17,19 @@ export default function LoginPage() {
   const { currentUser, login } = useAuth()
   console.log(currentUser)
 
+  const router = useRouter()
+
   const submitHandler = async event => {
     event.preventDefault()
 
     if(!email || !password) setError('Enter valid email and password')
 
     try {
-      return await login(email, password)      
+      // log them in
+      await login(email, password)
+      
+      // then redirect them to integrations page "/"
+      router.push('/')
     } catch (error) {
       console.log(error)
       setError('Incorrect email or password')
@@ -32,49 +40,69 @@ export default function LoginPage() {
 
   return (
     <div className='login-page-container'>
-      <Form className='login-form'>
-        <h1 className='login-title'>Login</h1>
 
-        <FloatingLabel
-          controlId="floatingInput"
-          label="Email address"
-          className="my-4"
-        >
+      {
+        currentUser
+        ?
+        (
+          <div className='already-logged-in-container'>
+            <h1>You Are Already Logged In</h1>
 
-          <Form.Control
-          type="email"
-          placeholder="name@example.com"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          />
+            <p>Click the button below to go to integrations page</p>
 
-        </FloatingLabel>
+            <Link href="/">
+              <a className='already-logged-in-btn'>Integrations Page</a>
+            </Link>
+          </div>
+        )
+        :
+        (
+          <Form className='login-form'>
+            <h1 className='login-title'>Login</h1>
+
+            <FloatingLabel
+            controlId="floatingInput"
+            label="Email address"
+            className="my-4"
+            >
+
+              <Form.Control
+              type="email"
+              placeholder="name@example.com"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              />
+
+            </FloatingLabel>
 
 
-        <FloatingLabel
-        controlId="floatingPassword"
-        label="Password"
-        className="my-4"
-        >
+            <FloatingLabel
+            controlId="floatingPassword"
+            label="Password"
+            className="my-4"
+            >
 
-          <Form.Control
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          />
+              <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              />
 
-        </FloatingLabel>
+            </FloatingLabel>
 
-        <Button
-        type="submit"
-        variant='primary'
-        onClick={submitHandler}
-        >
-          Log in
-        </Button>
+            <Button
+            type="submit"
+            variant='primary'
+            onClick={submitHandler}
+            >
+              Log in
+            </Button>
 
-      </Form>
+          </Form>
+        )
+      }
+
     </div>
   )
 }
--- blinq-challenge-code/pages/register.js
+++ blinq-challenge-code/pages/register.js
@@ -4,6 +4,8 @@ import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { useAuth } from '../context/AuthContext'
+import Link from 'next/link';
+import { useRouter } from 'next/router'
 
 
 
@@ -16,13 +18,19 @@ export default function RegisterPage() {
   const { currentUser, signup } = useAuth()
   console.log(currentUser)
 
+  const router = useRouter()
+
   const submitHandler = async event => {
     event.preventDefault()
 
     if(!email || !password) setError('Enter valid email and password')
 
     try {
-      return await signup(email, password)      
+      // signup function also logs user in
+      await signup(email, password)
+
+      // then redirect them to integrations page "/"
+      router.push('/')
     } catch (error) {
       console.log(error)
       setError('Incorrect email or password')
@@ -33,64 +41,84 @@ export default function RegisterPage() {
   
   return (
     <div className='register-page-container'>
-      <Form className='register-form'>
-        <h1 className='register-title'>Register</h1>
 
-        <FloatingLabel
-          controlId="floatingInput"
-          label="Email address"
-          className="my-4"
-        >
+      {
+        currentUser
+        ?
+        (
+          <div className='already-logged-in-container'>
+            <h1>You Are Already Logged In</h1>
 
-          <Form.Control
-          type="email"
-          placeholder="name@example.com"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          />
+            <p>Click the button below to go to integrations page</p>
 
-        </FloatingLabel>
+            <Link href="/">
+              <a className='already-logged-in-btn'>Integrations Page</a>
+            </Link>
+          </div>
+        )
+        :
+        (
+          <Form className='register-form'>
+            <h1 className='register-title'>Register</h1>
+
+            <FloatingLabel
+            controlId="floatingInput"
+            label="Email address"
+            className="my-4"
+            >
+
+              <Form.Control
+              type="email"
+              placeholder="name@example.com"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              />
+
+            </FloatingLabel>
 
 
-        <FloatingLabel
-        controlId="floatingPassword"
-        label="Password"
-        className="my-4"
-        >
+            <FloatingLabel
+            controlId="floatingPassword"
+            label="Password"
+            className="my-4"
+            >
 
-          <Form.Control
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          />
+              <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              />
 
-        </FloatingLabel>
+            </FloatingLabel>
 
-        <FloatingLabel
-        controlId="floatingPassword"
-        label="Confirm password"
-        className="my-4"
-        >
+            <FloatingLabel
+            controlId="floatingPassword"
+            label="Confirm password"
+            className="my-4"
+            >
 
-          <Form.Control
-          type="password"
-          placeholder="Password"
-          value={confirmPassword}
-          onChange={e => setConfirmPassword(e.target.value)}
-          />
+              <Form.Control
+              type="password"
+              placeholder="Password"
+              value={confirmPassword}
+              onChange={e => setConfirmPassword(e.target.value)}
+              />
 
-        </FloatingLabel>
+            </FloatingLabel>
 
-        <Button
-        type="submit"
-        variant='primary'
-        onClick={submitHandler}
-        >
-          Register
-        </Button>
+            <Button
+            type="submit"
+            variant='primary'
+            onClick={submitHandler}
+            >
+              Register
+            </Button>
 
-      </Form>
+          </Form>
+        )
+      }
+
     </div>
   )
 }
